242856541b8b34fdd442e9e5f73cd36722c48361
Brian Hazell <brianhazell@Brians-MacBook-Pro.local>
2023-08-11 18:25:48 -0700

    updated film entity to be contributed to by reviews subgraph

--- service-reviews/datasources/reviews-api.js
+++ service-reviews/datasources/reviews-api.js
@@ -3,8 +3,8 @@ const { RESTDataSource } = require("@apollo/datasource-rest");
 class ReviewsAPI extends RESTDataSource {
     baseURL = "https://api.nytimes.com/svc/movies/v2/"
 
-    getReview(search_string, apiKey) {
-        return this.get(`reviews/search.json?query=${search_string}&api-key=${apiKey}`);
+    getReview(search_string) {
+        return this.get(`reviews/search.json?query=${search_string}&api-key=xXtd64C7bDJjRup1m4G4TJFoSe9KWXmp`);
     }
 
 }
--- service-reviews/resolvers.js
+++ service-reviews/resolvers.js
@@ -1,12 +1,57 @@
+function extractNYTResults(results) {
+    if (results == null) {
+        return null 
+        // Add logic to accomdate NYT results for 'A New Hope'
+    } else if (results.length > 1) {
+        return results[9]
+    } else {
+        return results[0]
+    }
+}
+// Generates an ID field based of the display title based on the SWAPI ID Needs
+function mapNYTDisplayTitleToSWAPIID(displayTitle) {
+    if (displayTitle.includes("Attack of the Clones")) {
+        return 5
+    } else if (displayTitle == "Star Wars") {
+        return 1
+    } else if (displayTitle.includes("The Empire Strikes Back")) {
+        return 2
+    } else if (displayTitle.includes("Return of the Jedi")) {
+        return 3
+    } else if (displayTitle.includes("The Phantom Menace")) {
+        return 4
+    } else if (displayTitle.includes("Revenge of the Sith")) {
+        return 6
+    } else {
+        return null
+    }
+
+}
+// Generates a search string based of the SWAPI ID to search the NYT API
+function mapSWAPIIDToNYTSearchString(id) {
+    if (id == 1) {
+        return "Star Wars"
+    } else if (id == 2) {
+        return "Empire Strikes Back"
+    } else if (id == 3) {
+        return "Return of the Jedi"
+    } else if (id == 4) {
+        return "Phantom Menace"
+    } else if (id == 5) {
+        return "Attack of the Clones"
+    } else if (id == 6) {
+        return "Revenge of the Sith"
+    }
+}
 
 const resolvers = {
     Query: {
-      getReview: (_, { title, apiKey }, { dataSources }) => {
+      getReview: (_, { searchTitle }, { dataSources }) => {
         // Add logic to accomdate NYT results for 'A New Hope'
-        if (title == "A New Hope"){
-            title = "Star Wars"
+        if (searchTitle == "A New Hope"){
+            searchTitle = "Star Wars"
         }
-        return dataSources.reviewsAPI.getReview(title, apiKey);
+        return dataSources.reviewsAPI.getReview(searchTitle);
       },
     },
 
@@ -21,15 +66,9 @@ const resolvers = {
         },
 
         nodes: ({ results }) => {
-            if (results == null) {
-                return null 
-                // Add logic to accomdate NYT results for 'A New Hope'
-            } else if (results.length > 1) {
-                results = results[9]
-            } else {
-                results = results[0]
-            }
+            results = extractNYTResults(results)
                 return {
+                    displayTitle: results["display_title"],
                     mpaaRating: results["mpaa_rating"],
                     criticsPick: results["critics_pick"],
                     summaryShort: results["summary_short"],
@@ -37,6 +76,26 @@ const resolvers = {
                 }
         },
     },
+
+    Film: {
+        id: ({ displayTitle }) => {
+            return mapNYTDisplayTitleToSWAPIID(displayTitle)
+        },
+
+        __resolveReference: async ({id}, { dataSources}) => {
+            let search_query = mapSWAPIIDToNYTSearchString(id)
+            let search_results = await dataSources.reviewsAPI.getReview(search_query);
+            results = search_results["results"]
+            results = extractNYTResults(results)
+            return {
+                displayTitle: results["display_title"],
+                mpaaRating: results["mpaa_rating"],
+                criticsPick: results["critics_pick"],
+                summaryShort: results["summary_short"],
+                articleURL: results["link"]["url"]
+            }
+        }
+    }
 
   };
 
